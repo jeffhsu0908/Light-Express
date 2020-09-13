6b50bce8822784ac39264633448b7cef37335f95
jeff hsu <jeffhsu@jeffs-MacBook-Pro.local>
2020-09-13 16:36:22 -0400

    login page

--- src/features/login-page.js
+++ src/features/login-page.js
@@ -1,21 +1,39 @@
-/* Login Page
+/* User Sign Up Form
  *
  * author: Jeff Hsu
  */
 
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View} from 'react-native';
 import {TextInput, Button} from 'react-native-paper';
 
-export default class LoginPage extends Component {
-  render() {
-    return (
-      <View>
-        <TextInput label="Username" />
-        <TextInput label="Password" />
-        <Button icon="camera" mode="contained">Login</Button>
-        <Button icon="camera" mode="contained">Sign Up</Button>
-      </View>
-    );
+function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onPressLoginButton = () => {
+    // TODO: implement user press login functionality
+  };
+
+  const onPressSignUpButton = () => {
+    // TODO: redirect user to sign up form
   }
+  return (
+    <View>
+      <TextInput
+        label="Username"
+        onChangeText={(value) => setUsername(value)}
+      />
+      <TextInput
+        label="Password"
+        onChangeText={(value) => setPassword(value)}
+      />
+      <Button mode="contained" onPress={onPressLoginButton}>
+        Login
+      </Button>
+      <Button mode="contained" onPress={onPressSignUpButton}>Sign Up</Button>
+    </View>
+  );
 }
+
+export default LoginPage;
